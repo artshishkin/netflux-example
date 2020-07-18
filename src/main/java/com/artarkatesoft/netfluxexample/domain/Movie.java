b9da37a5752a9cd3e829b3308c2a287e4fb101e0
admin <d.art.shishkin@gmail.com>
2020-07-18 11:10:27 +0300

    Modified Lombok Annotations of domain models

--- src/main/java/com/artarkatesoft/netfluxexample/domain/Movie.java
+++ src/main/java/com/artarkatesoft/netfluxexample/domain/Movie.java
@@ -1,11 +1,15 @@
 package com.artarkatesoft.netfluxexample.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
     private String id;
 
