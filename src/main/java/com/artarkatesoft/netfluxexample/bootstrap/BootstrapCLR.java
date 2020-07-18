23d126ab2ee26daeac450662d3783def547b236b
admin <d.art.shishkin@gmail.com>
2020-07-18 11:39:18 +0300

    Initializing Data With Spring Boot Command Line Runner (16)

---
+++ src/main/java/com/artarkatesoft/netfluxexample/bootstrap/BootstrapCLR.java
@@ -0,0 +1,33 @@
+package com.artarkatesoft.netfluxexample.bootstrap;
+
+import com.artarkatesoft.netfluxexample.domain.Movie;
+import com.artarkatesoft.netfluxexample.repositories.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class BootstrapCLR implements CommandLineRunner {
+
+    private final MovieRepository movieRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        //delete all data
+        movieRepository.deleteAll().block();
+
+        Flux.just("Silence of the Lambdas", "AEon Flux", "Enter the Mono<Void>", "The Fluxxinator",
+                "Back to the Future", "Meet the Fluxes", "Lord of the Fluxes")
+                .map(title -> new Movie(UUID.randomUUID().toString(), title))
+                .flatMap(movieRepository::save)
+                .subscribe(null, null,
+                        () -> movieRepository.findAll().subscribe(System.out::println))
+        ;
+
+    }
+}
